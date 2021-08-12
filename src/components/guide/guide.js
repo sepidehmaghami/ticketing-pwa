8941a33d5dca700ebe2e28f58a615819c2eae167
M_Ansari_Astaneh <mobinaansariit@gmail.com>
2021-08-12 21:17:37 +0430

    third edition of guide page

--- src/components/guide/guide.js
+++ src/components/guide/guide.js
@@ -3,6 +3,15 @@ import Head from '../header/header';
 import { Layout ,Breadcrumb } from 'antd';
 import {Helmet} from "react-helmet";
 const {  Header,Content } = Layout;
+import { Card, Col, Row } from 'antd';
+// const { Meta } = Card;
+import { PhoneOutlined , SendOutlined , FileTextOutlined } from '@ant-design/icons';
+import Maghami from '../../assets/Maghami.jpg';
+import Ansari from '../../assets/Ansari.jpg';
+import { Typography } from 'antd';
+const { Paragraph } = Typography;
+// import Heidari from '../../assets/Heidari.jpg';
+
 
 function Guide ()  {
     return (
@@ -21,29 +30,64 @@ function Guide ()  {
          {/* <Breadcrumb.Item style={{ fontWeight: 'bold' }}>Guide</Breadcrumb.Item> */}
         </Breadcrumb>
         <div className="site-layout-content">
-         <h2>Arashweb</h2>
-         <h4>Company : <a href="https://arashweb.com/fa/blog/">Arashweb</a></h4>
-          Email : <a href="info@arashweb.ir">Arashweb - Email</a> <br/>
-          Tel : +98 (513) 848-2949 <br/>
-          Phone : +98 (915) 715-3194 <br/>
-          Address: Ebn Sina St. #211, Floor 2, Office A, Mashhad, Iran <br/><br/>
-          <h4>FrontEnd by : <a href={'./profile'}> Mrs. Mobina Ansari Astaneh</a></h4>
-          Email : <a href="mobinaansariit@gmail.com">Ansari - Email</a> <br/>
-          Telegram : <a href="@M_Ansari_Astaneh">Ansari - Telegram</a> <br/>
-          Linkedin : <a href="https://www.linkedin.com/in/mobina-ansari-astaneh-431981213/">Ansari - Linkedin</a> <br/>
-          Phone : +98 915 445 0822
-          <br/><br/>
-          <h4>BackEnd by : <a href={'./profile'}> Mr. Ata Attarian</a> </h4>
-          Email : <a href="ata.attarian78@gmail.com">Attarian - Email</a> <br/>
-          Telegram : <a href="">Attarian - Telegram</a> <br/>
-          Linkedin : <a href="">Attarian - Linkedin</a>
-          <br/><br/>
-          <h4>Tested by : <a href={'./profile'}> Mrs. Sepideh Darban Maghami </a></h4>
-          Email : <a href="sepidehmaghamiit@gmail.com">Maghami - Email</a> <br/>
-          Telegram : <a href="@S_darban_maghami">Maghami - Telegram</a> <br/>
-          Linkedin : <a href="https://www.linkedin.com/in/sepideh-darban-maghami-7bbb4b1b9/">Maghami - Linkedin</a> <br/>
-          Phone : +98 915 066 0935
-          <br/><br/>
+         <h2 className="arashweb"><a href="https://arashweb.com/fa/blog/">Arashweb Company</a></h2>
+         <div className="or"></div>
+          <div className="site-card-wrapper">
+            <Row gutter={32}>
+            <Col span={6}>
+                <Card title="CEO" bordered={false}
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="Parham Heidari" src="https://www.chat.arashweb.com/avatar/parham" />}
+                  >
+                    <h6><a href={'./profile'}>Parham Heidari</a></h6>
+                    <FileTextOutlined style={{ padding: '1px 6px 4px 0px' }}/>Email : <Paragraph copyable>heidari1377@gmail.com</Paragraph>
+                    <PhoneOutlined style={{ padding: '1px 6px 4px 0px' }}/>Phone Number : <Paragraph copyable>+98 (513) 848-2949</Paragraph>
+                    <SendOutlined style={{ padding: '1px 6px 4px 0px' }}/>Telegram : <Paragraph copyable>@parhamheidari</Paragraph>
+                    {/* <LinkedinOutlined style={{ padding: '1px 6px 4px 0px' }}/><a href="https://www.linkedin.com/in/parhamheidari/">Linkedin-Heidari</a>*/}
+                    </Card>
+              </Col>
+              <Col span={6}>
+                <Card title="Front-end Developer" bordered={false}
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="Mobina Ansari" src={Ansari} />}
+                  >
+                    <h6><a href={'./profile'}>Mobina Ansari Astaneh</a></h6>
+                    <FileTextOutlined style={{ padding: '1px 6px 4px 0px' }}/>Email : <Paragraph copyable>mobinaansariit@gmail.com</Paragraph>
+                    <PhoneOutlined style={{ padding: '1px 6px 4px 0px' }}/>Phone Number : <Paragraph copyable>+98 915 445 0822</Paragraph>
+                    <SendOutlined style={{ padding: '1px 6px 4px 0px' }}/>Telegram : <Paragraph copyable>@M_Ansari_Astaneh</Paragraph>
+                    {/* <LinkedinOutlined style={{ padding: '-10px 5px 4px 0px' }}/><a href="https://www.linkedin.com/in/mobina-ansari-astaneh-431981213/">Linkedin-Ansari</a>*/}
+                </Card>
+              </Col>
+              <Col span={6}>
+                <Card title="Back-end Developer" bordered={false}
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="Ata Attarian" src="https://www.chat.arashweb.com/avatar/ataattarian" />}
+                  >
+                    <h6><a href={'./profile'}>Ata Attarian</a></h6>
+                    <FileTextOutlined style={{ padding: '1px 6px 4px 0px' }}/>Email : <Paragraph copyable>ata.attarian78@gmail.com</Paragraph>
+                    <PhoneOutlined style={{ padding: '1px 6px 4px 0px' }}/>Phone Number : <Paragraph copyable>+98 (915) 715-3194</Paragraph>
+                    <SendOutlined style={{ padding: '1px 6px 4px 0px' }}/>Telegram : <Paragraph copyable>@atta_attarian</Paragraph>
+                    {/* <LinkedinOutlined style={{ padding: '-10px 5px 4px 0px' }}/><a href="https://www.linkedin.com/in/atta-attarian-741919193/">Attarian - Linkedin</a> */}
+                </Card>
+              </Col>
+              <Col span={6}>
+                <Card title="Suppoter" bordered={false}
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="Sepideh Maghami" src={Maghami} />}
+                  >
+                <h6><a href={'./profile'}>Sepideh Darban Maghami</a></h6>
+                    <FileTextOutlined style={{ padding: '1px 6px 4px 0px' }}/>Email : <Paragraph copyable>sepideh@gmail.com</Paragraph>
+                    <PhoneOutlined style={{ padding: '1px 6px 4px 0px' }}/>Phone Number : <Paragraph copyable>+98 915 066 0935</Paragraph>
+                    <SendOutlined style={{ padding: '1px 6px 4px 0px' }}/>Telegram : <Paragraph copyable>@S_darban_maghami</Paragraph>
+                    {/* <LinkedinOutlined style={{ padding: '-10px 5px 4px 0px' }}/><a href="https:https://www.linkedin.com/in/sepideh-darban-maghami-7bbb4b1b9/">Linkedin-Ansari</a>*/}
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </div>
       </Content>
      
